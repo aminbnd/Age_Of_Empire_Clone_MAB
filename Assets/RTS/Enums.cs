14fdf5aa0d954d6627110d1181aa0494a8e5de67
Amin Bondi <2212@holbertonschool.com>
2022-04-27 19:09:35 +0100

    0.5 * part 12

--- Assets/RTS/Enums.cs
+++ Assets/RTS/Enums.cs
@@ -1,5 +1,6 @@
 namespace RTS
 {
     public enum CursorState { Select, Move, Attack, PanLeft, PanRight, PanUp, PanDown, Harvest, RallyPoint }
-    public enum ResourceType { Money, Power, Food, Wood }
+    public enum ResourceType { Money, Power, Food, Wood, Ore, Unknown }
+
 }
